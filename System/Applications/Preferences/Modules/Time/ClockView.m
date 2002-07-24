b5aa2cbbb34405946a7565fdc91d9bfbad318e70
Jeff Teunissen <deek@d2dc.net>
2002-07-24 13:57:23 +0000

    Yay, it's finally working! Add a Time & Date module, which also happens to be a clock. This is the last known thing needed for 1.3.0 to be released.

---
+++ System/Applications/Preferences/Modules/Time/ClockView.m
@@ -0,0 +1,349 @@
+/*
+	ClockView.m
+
+	A digital clock view
+
+	Copyright (C) 2002 Dusk to Dawn Computing, Inc.
+	Additional copyrights here
+
+	Author: Jeff Teunissen <deek@d2dc.net>
+	Date:	29 Jun 2002
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation; either version 2 of
+	the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+*/
+static const char rcsid[] = 
+	"$Id$";
+
+#ifdef HAVE_CONFIG_H
+# include "Config.h"
+#endif
+
+#import <Foundation/NSCalendarDate.h>
+#import <Foundation/NSTimer.h>
+
+#import <AppKit/NSApplication.h>
+#import <AppKit/NSEvent.h>
+#import <AppKit/NSImage.h>
+
+#import "ClockView.h"
+
+#define NEW_IMAGE(a) \
+	[[NSImage alloc] initWithContentsOfFile: [this_bundle pathForImageResource: (a)]]
+
+static	NSBundle	*this_bundle = nil;
+
+// unchanging images
+static	NSImage		*mask = nil;
+static	NSImage		*colon = nil;
+
+// changing images
+static	NSImage		*min1 = nil, *min2 = nil;
+static	NSImage		*hour1 = nil, *hour2 = nil, *ampm = nil;
+static	NSImage		*dow = nil,	*dom1 = nil, *dom2 = nil;
+static	NSImage		*month = nil;
+
+@implementation ClockView (Private)
+
++ (void) initialize
+{
+	this_bundle = [NSBundle bundleForClass: self];
+	mask = NEW_IMAGE (@"Mask");
+	colon = NEW_IMAGE (@"Time-Colon");
+}
+
+- (BOOL) acceptsFirstMouse: (NSEvent *) anEvent
+{
+	return YES;
+}
+
+- (void) mouseDown: (NSEvent *) event
+{
+	if ([event clickCount] >= 2)
+		[NSApp unhide: self];
+}
+
+- (void) awakeFromNib
+{
+	[self setDrawsTile: YES];
+	[self setDate: [NSCalendarDate calendarDate]];
+	timer = [NSTimer scheduledTimerWithTimeInterval: 0.5
+											 target: self
+										   selector: @selector(update:)
+										   userInfo: nil
+											repeats: YES];
+}
+
+- (void) displayIfNeeded
+{
+	if ([self needsDisplay])
+		[self display];
+}
+
+- (BOOL) isOpaque
+{
+	if (drawsTile)
+		return YES;
+	return NO;
+}
+
+@end
+
+@implementation ClockView
+
+- (id) init
+{
+	if (!(self = [super init]))
+		return nil;
+
+	return self;
+}
+- (void) dealloc
+{
+	[timer invalidate];
+	[timer release];
+
+	[mask release];
+	[hour1 release];
+	[hour2 release];
+	[colon release];
+	[min1 release];
+	[min2 release];
+	[dow release];
+	[dom1 release];
+	[dom2 release];
+	[month release];
+	[tileImage release];
+	[super dealloc];
+}
+
+- (void) setDate: (NSCalendarDate *) aDate
+{
+	static int	lastdom = -1, lastmonth = -1, lasthour = -1, lastmin = -1;
+	static BOOL	last24 = NO;
+
+	int			_hour = [aDate hourOfDay];
+	int			_min = [aDate minuteOfHour];
+	int			_dow = [aDate dayOfWeek];
+	int			_dom = [aDate dayOfMonth];
+	int			_month = [aDate monthOfYear];
+	
+	BOOL	dateChanged = NO;
+
+	if (lastdom != _dom) {
+		lastdom = _dom;
+		dateChanged = YES;
+
+		[dow release];
+		[dom1 release];
+		[dom2 release];
+		dow = NEW_IMAGE (([NSString stringWithFormat: @"Day-%d", _dow]));
+		dom1 = NEW_IMAGE (([NSString stringWithFormat: @"Date-%d", _dom / 10]));
+		dom2 = NEW_IMAGE (([NSString stringWithFormat: @"Date-%d", _dom % 10]));
+	}
+
+	if (lastmonth != _month) {
+		lastmonth = _month;
+		dateChanged = YES;
+
+		[month release];
+		month = NEW_IMAGE (([NSString stringWithFormat: @"Month-%02d", _month]));
+	}
+
+	if (lasthour != _hour || last24 != use24Hours) {
+		lasthour = _hour;
+		last24 = use24Hours;
+
+		dateChanged = YES;
+
+		[hour1 release];
+		[hour2 release];
+		hour1 = nil;
+		if (use24Hours) {
+			hour1 = NEW_IMAGE (([NSString stringWithFormat: @"Time-%d", _hour / 10]));
+			hour2 = NEW_IMAGE (([NSString stringWithFormat: @"Time-%d", _hour % 10]));
+		} else {
+			[ampm release];
+
+			if (_hour > 11) {
+				ampm = NEW_IMAGE (@"Time-P");
+				_hour -= 12;
+			} else {
+				ampm = NEW_IMAGE (@"Time-A");
+			}
+
+			if (_hour == 0) {
+				_hour = 12;
+			}
+			if (_hour / 10)
+				hour1 = NEW_IMAGE (([NSString stringWithFormat: @"Time-%d", _hour / 10]));
+			hour2 = NEW_IMAGE (([NSString stringWithFormat: @"Time-%d", _hour % 10]));
+		}
+	}
+
+	if (lastmin != _min) {
+		lastmin = _min;
+		dateChanged = YES;
+
+		[min1 release];
+		[min2 release];
+		min1 = NEW_IMAGE (([NSString stringWithFormat: @"Time-%d", _min / 10]));
+		min2 = NEW_IMAGE (([NSString stringWithFormat: @"Time-%d", _min % 10]));
+	}
+
+	if (dateChanged)
+		[self setNeedsDisplay: YES];
+}
+
+- (void) drawRect: (NSRect) aRect
+{
+	NSRect	maskRect;
+	NSSize	maskSize;
+	NSPoint	maskLoc;
+	NSPoint	location;
+	NSRect	bottomInsideRect;
+	NSRect	topInsideRect;
+	int		timeWidth;
+
+	maskSize = [mask size];
+	maskRect = NSInsetRect(aRect, (aRect.size.width - maskSize.width) / 2,
+								 (aRect.size.height - maskSize.height) / 2);
+	maskLoc = NSMakePoint (maskRect.origin.x, maskRect.origin.y);
+
+	// Rect defining the inside of the "date" area
+	bottomInsideRect = NSMakeRect (maskRect.origin.x,
+								   maskRect.origin.y, 54, 35);
+
+	// Rect defining the inside of the "time" area
+	topInsideRect = NSMakeRect (maskRect.origin.x,
+								maskRect.origin.y + 40, 54, 15);
+
+	// draw the tile and mask
+	if (drawsTile && tileImage)
+		[tileImage compositeToPoint: NSZeroPoint operation: NSCompositeSourceOver];
+	[mask compositeToPoint: maskLoc operation: NSCompositeSourceOver];
+
+	// day of week
+	location.x = bottomInsideRect.origin.x +
+				(bottomInsideRect.size.width / 2 - 
+				(([dow size].width + [month size].width)/2));
+	location.y = bottomInsideRect.origin.y + ([dom2 size].height - 3);
+	[dow compositeToPoint: location operation: NSCompositeSourceOver];
+	location.x += [dow size].width + 2;
+
+	// month name
+	[month compositeToPoint: location operation: NSCompositeSourceOver];
+
+	// day of month
+	location.x = bottomInsideRect.origin.x +
+				(bottomInsideRect.size.width / 2 - 
+				(([dom1 size].width + [dom2 size].width)/2) + 1) + 1;
+	location.y = bottomInsideRect.origin.y;
+	[dom1 compositeToPoint: location operation: NSCompositeSourceOver];
+
+	location.x += [dom1 size].width + 1;
+	[dom2 compositeToPoint: location operation: NSCompositeSourceOver];
+
+	/*
+		Draw the time
+	*/
+	if (!use24Hours) {	// skew the clock left for AM/PM display
+		topInsideRect.size.width -= [ampm size].width + 1;
+		location.x = topInsideRect.origin.x + topInsideRect.size.width;
+		location.y = topInsideRect.origin.y +
+					(topInsideRect.size.height / 2 - 
+					([ampm size].height / 2)) + 1;
+		[ampm compositeToPoint: location operation: NSCompositeSourceOver];
+	}
+
+	timeWidth = 1 + [hour2 size].width + 1
+				+ [colon size].width + 1
+				+ [min1 size].width + 1
+				+ [min2 size].width;
+	if (hour1)
+		timeWidth += [hour1 size].width + 1;
+
+	topInsideRect = NSInsetRect (topInsideRect,
+								(topInsideRect.size.width - timeWidth) / 2,
+								0);
+	
+	location.x = topInsideRect.origin.x;
+
+	if (hour1) {
+		location.y = topInsideRect.origin.y +
+					(topInsideRect.size.height / 2 - 
+					([hour1 size].height / 2)) + 1;
+		[hour1 compositeToPoint: location operation: NSCompositeSourceOver];
+		location.x += [hour1 size].width + 1;
+	}
+
+	location.y = topInsideRect.origin.y +
+				(topInsideRect.size.height / 2 - 
+				([hour2 size].height / 2)) + 1;
+	[hour2 compositeToPoint: location operation: NSCompositeSourceOver];
+	location.x += [hour2 size].width + 1;
+
+	location.y = topInsideRect.origin.y +
+				(topInsideRect.size.height / 2 - 
+				([colon size].height / 2));
+	[colon compositeToPoint: location operation: NSCompositeSourceOver];
+	location.x += [colon size].width + 1;
+
+	location.y = topInsideRect.origin.y +
+				(topInsideRect.size.height / 2 - 
+				([min1 size].height / 2)) + 1;
+	[min1 compositeToPoint: location operation: NSCompositeSourceOver];
+	location.x += [min1 size].width + 1;
+
+	location.y = topInsideRect.origin.y +
+				(topInsideRect.size.height / 2 - 
+				([min2 size].height / 2)) + 1;
+	[min2 compositeToPoint: location operation: NSCompositeSourceOver];
+}
+
+- (void) update: (id) sender
+{
+	[self setDate: [NSCalendarDate calendarDate]];
+}
+
+- (BOOL) drawsTile
+{
+	return drawsTile;
+}
+
+- (void) setDrawsTile: (BOOL) flag
+{
+	drawsTile = flag;
+
+	if (flag)
+		tileImage = [NSImage imageNamed: @"common_Tile"];
+	else
+		DESTROY (tileImage);
+}
+
+- (BOOL) uses24Hours
+{
+	return use24Hours;
+}
+
+- (void) setUses24Hours: (BOOL) flag
+{
+	use24Hours = flag;
+}
+
+@end
---
+++ System/Applications/Preferences/Modules/Time/Time.m
@@ -0,0 +1,166 @@
+/*
+	Time.m
+
+	Controller class for this bundle
+
+	Copyright (C) 2002 Dusk to Dawn Computing, Inc.
+	Additional copyrights here
+
+	Author: Jeff Teunissen <deek@d2dc.net>
+	Date:	29 Jun 2002
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation; either version 2 of
+	the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+*/
+static const char rcsid[] = 
+	"$Id$";
+
+#ifdef HAVE_CONFIG_H
+# include "Config.h"
+#endif
+
+#import <Foundation/NSDebug.h>
+#import <Foundation/NSUserDefaults.h>
+#import <Foundation/NSValue.h>
+
+#import <AppKit/NSApplication.h>
+#import <AppKit/NSButton.h>
+#import <AppKit/NSNibLoading.h>
+#import <AppKit/NSOpenPanel.h>
+
+#import "Time.h"
+#import "ClockView.h"
+
+@interface Time (Private)
+- (void) updateUI;
+@end
+
+@implementation Time (Private)
+
+static NSUserDefaults		*defaults = nil;
+static id <PrefsController>	controller = nil;
+static NSWindow				*iconWin = nil;
+static ClockView			*clock = nil;
+
+- (void) updateUI
+{
+	[clockUses24HoursButton setIntValue: [defaults boolForKey: @"ClockUses24Hours"]];
+	[localTimeZoneField setStringValue: [defaults stringForKey: @"Local Time Zone"]];
+	[clock setUses24Hours: [defaults boolForKey: @"ClockUses24Hours"]];
+	[view setNeedsDisplay: YES];
+}
+
+@end	// Time (Private)
+
+@implementation Time
+
+static Time						*sharedInstance = nil;
+static id <PrefsApplication>	owner = nil;
+
+- (id) initWithOwner: (id <PrefsApplication>) anOwner
+{
+	if (sharedInstance) {
+		[self dealloc];
+	} else {
+		NSDictionary		*dict;
+
+		self = [super init];
+		owner = anOwner;
+		controller = [owner prefsController];
+		defaults = [NSUserDefaults standardUserDefaults];
+		dict = [[NSDictionary alloc] initWithObjectsAndKeys:
+				[NSNumber numberWithBool: NO], @"ClockUses24Hours",
+				nil];
+
+		if (![NSBundle loadNibNamed: @"Time" owner: self]) {
+			NSLog (@"Time: Could not load nib \"Time\", aborting.");
+			[self dealloc];
+			return nil;
+		}
+
+		[defaults registerDefaults: dict];
+		[controller registerPrefsModule: self];
+		// window can be any size, as long as it's 486x228 :)
+		view = [window contentView];
+
+		// Let's be mean to the app, taking its icon away
+		iconWin = [NSApp iconWindow];
+		clock = [[ClockView alloc] init];
+		[clock setUses24Hours: [defaults boolForKey: @"ClockUses24Hours"]];
+		
+		[iconWin setContentView: clock];
+		[clock awakeFromNib];
+
+		[view retain];
+
+		[self updateUI];
+
+		sharedInstance = self;
+	}
+	return sharedInstance;
+}
+
+- (void) showView: (id) sender;
+{
+	[controller setCurrentModule: self];
+	[view setNeedsDisplay: YES];
+}
+
+- (NSView *) view
+{
+	return view;
+}
+
+- (NSString *) buttonCaption
+{
+	return @"Time & Date Preferences";
+}
+
+- (NSImage *) buttonImage
+{
+	return [NSImage imageNamed: @"PrefsIcon_Time"];
+}
+
+- (SEL) buttonAction
+{
+	return @selector(showView:);
+}
+
+/*
+	Action methods
+*/
+- (IBAction) clockUses24HoursChanged: (id) sender
+{
+	[defaults setBool: [sender intValue] forKey: @"ClockUses24Hours"];
+	[defaults synchronize];
+	[self updateUI];
+}
+
+- (IBAction) localTimeFieldChanged: (id) sender
+{
+	NSMutableDictionary	*globals = nil;
+
+	globals = [[defaults persistentDomainForName: @"NSGlobalDomain"] mutableCopy];
+	[globals setObject: [sender stringValue] forKey: @"Local Time Zone"];
+	[defaults setPersistentDomain: globals forName: NSGlobalDomain]; \
+	[defaults synchronize];
+	[globals release];
+	[self updateUI];
+}
+
+@end	// Time
