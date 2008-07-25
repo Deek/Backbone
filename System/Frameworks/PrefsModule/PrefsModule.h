7cdd3fb8473cef526ffc6a6396cf1fe279451cab
Jeff Teunissen <deek@d2dc.net>
2008-07-25 01:58:49 -0400

    licensing cleanups
    Since there is no universal license that the entire project is under, collect
    all the different licenses together in one place and use symlinks to show
    which components are under which license(s).
    
    As well, clarify that PrefsModule is LGPL2+ and not GPLv2+.

--- System/Frameworks/PrefsModule/PrefsModule.h
+++ System/Frameworks/PrefsModule/PrefsModule.h
@@ -3,24 +3,24 @@
 
 	Definitions for all Preferences modules
 
-	Copyright (C) 2003 Dusk to Dawn Computing, Inc.
+	Copyright (C) 2003, 2004 Dusk to Dawn Computing
 
 	Author: Jeff Teunissen <deek@d2dc.net>
 	Date:	11 Nov 2001
 
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License as
-	published by the Free Software Foundation; either version 2 of
-	the License, or (at your option) any later version.
+	This framework is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public License
+	as published by the Free Software Foundation; either version 2.1
+	of the License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
+	This framework is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-	See the GNU General Public License for more details.
+	See the GNU Lesser General Public License for more details.
 
-	You should have received a copy of the GNU General Public
-	License along with this program; if not, write to:
+	You should have received a copy of the GNU Lesser General Public
+	License along with this framework; if not, write to:
 
 		Free Software Foundation, Inc.
 		59 Temple Place - Suite 330
@@ -68,7 +68,6 @@
 	Returns the module currently be displayed, or nil if no module is
 	currently being displayed.
 */
-
 - (id) currentModule;
 
 /*
